c7edc67273c42af4af6702bc49c960e83b958d3b
d363f86b <d363f86b@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-19 16:35:46 +0000

    Partial patch for the broken download links. More to come.
    git-svn-id: https://plugins.svn.wordpress.org/bulk-photo-to-product-importer-extension-for-woocommerce/trunk@951621 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bptpi.php
+++ bptpi.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.theportlandcompany.com/shop/custom-web-applications/bulk-
 Description: This Plugin is an extension to WooCommerce and enables users to bulk import photos, which are automatically converted into Products.
 Author: The Portland Company, Designed by Spencer Hill, Coded by Redeye Adaya
 Author URI: http://www.theportlandcompany.com
-Version: 2.2.6
+Version: 2.2.7
 Copyright: 2013 The Portland Company 
 License: GPL v3
 */
@@ -19,7 +19,7 @@ class PTP_Importer {
     /**
      * @var string
      */
-    public $version = '2.2.6';
+    public $version = '2.2.7';
 
     /**
      * @var string

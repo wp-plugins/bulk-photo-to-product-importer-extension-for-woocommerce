b53e2873a98c6203aa9cdd15e21282b9dba3899a
d363f86b <d363f86b@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 00:04:08 +0000

    Revisions requested by WordPress moderators.
    git-svn-id: https://plugins.svn.wordpress.org/bulk-photo-to-product-importer-extension-for-woocommerce/trunk@1107831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions.php
+++ includes/functions.php
@@ -508,80 +508,14 @@ function ptp_our_products_counter() {
  * @param string $serial_key
  * @return array
  */
-function ptp_is_valid_serial_key( $serial_key ) {
-    // API URL
-    $url = 'http://apps.theportlandcompany.com/sts/api/v1/item/'.$serial_key;
-
-    // cURL Resource 
-    $ch = curl_init();
-
-    // Set URL 
-    curl_setopt( $ch, CURLOPT_URL, $url );
-
-    // Tell cURL to return the output 
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-    // Tell cURL NOT to return the headers 
-    curl_setopt( $ch, CURLOPT_HEADER, false );
-
-    // Execute cURL, Return Data
-    $data = curl_exec( $ch );
-
-    // Check HTTP Code
-    $status = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-
-    // Close cURL Resource 
-    curl_close( $ch );
-
-    $res = json_decode( $data );
-
-    // If Response is not 200
-    if ( $status == 200 && !$res->error ) {
-        return array( 'success' => true, 'item_name' => $res->item_name );
-    }
-
-    return array( 'success' => false, 'message' => $res->message );
-}
+function ptp_is_valid_serial_key( $serial_key ) {}
 
 /**
  * Check if this IP is registered in the API
  *
  * @return array
  */
-function ptp_is_ip_registered() {
-    // API URL
-    $url = 'http://apps.theportlandcompany.com/sts/api/v1/ip/'.$_SERVER['SERVER_ADDR'];
-
-    // cURL Resource 
-    $ch = curl_init();
-
-    // Set URL 
-    curl_setopt( $ch, CURLOPT_URL, $url );
-
-    // Tell cURL to return the output 
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-    // Tell cURL NOT to return the headers 
-    curl_setopt( $ch, CURLOPT_HEADER, false );
-
-    // Execute cURL, Return Data
-    $data = curl_exec( $ch );
-
-    // Check HTTP Code
-    $status = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-
-    // Close cURL Resource 
-    curl_close( $ch );
-
-    $res = json_decode( $data );
-
-    // If Response is not 200
-    if ( $status == 200 && !$res->error ) {
-        return array( 'success' => true );
-    }
-
-    return array( 'success' => false, 'message' => $res->message );
-}
+function ptp_is_ip_registered() {}
 
 /**
  * Register IP Address and Domain Name where this plugin is installed
@@ -589,53 +523,7 @@ function ptp_is_ip_registered() {
  * @param string $serial_key
  * @return array
  */
-function ptp_register_server_info( $serial_key ) {
-    global $ptp_importer;
-
-    // API URL
-    $url = 'http://apps.theportlandcompany.com/sts/api/v1/ip';
-
-    // $_GET Parameters to Send 
-    $params = array( 
-        'item_name' => $ptp_importer->plugin_name,
-        'serial_key' => $serial_key, 
-        'ip' => $_SERVER['SERVER_ADDR'], 
-        'domain_name' => $_SERVER['SERVER_NAME'] 
-    );
-
-    // Resource
-    $ch = curl_init();
-
-    // Set URL
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    // Tell cURL that this is a POST
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    // Bind params
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-
-    // Receive server reponse
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Execute cURL
-    $data = curl_exec($ch);
-
-    // Check HTTP Code
-    $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    // Close cURL Resource 
-    curl_close($ch);
-
-    $res = json_decode( $data );
-
-    // If Response is not 200
-    if ( $status == 200 && !$res->error ) {
-        return array( 'success' => true );
-    }
-
-    return array( 'success' => false, 'message' => $res->message );
-}
+function ptp_register_server_info( $serial_key ) {}
 
 /**
  * Deactivate this plugin
@@ -643,43 +531,7 @@ function ptp_register_server_info( $serial_key ) {
  * @param string $serial_key
  * @return array
  */
-function ptp_deactivate($serial_key) {
-    // API URL
-    $url = 'http://apps.theportlandcompany.com/sts/api/v1/ip/'.$_SERVER['SERVER_ADDR'].'_'.$serial_key;
-
-    // Resource
-    $ch = curl_init();
-
-    // Set URL
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    // Delete
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-
-    // Follow location
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-    // Do not return http headers
-    curl_setopt($ch, CURLOPT_HEADER, 0);  
-
-    // Receive server reponse
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
-
-    // Execute cURL
-    $data = curl_exec($ch);
-
-    // Check HTTP Code
-    $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    $res = json_decode( $data );
-
-    // If Response is not 200
-    if ( $status == 200 && !$res->error ) {
-        return array( 'success' => true );
-    }
-
-    return array( 'success' => false, 'message' => $res->message );
-}
+function ptp_deactivate($serial_key) {}
 
 /**
  * Checks if plugin is active
@@ -695,18 +547,7 @@ function ptp_is_active() {
  *
  * @return array
  */
-function ptp_verify() {
-    // Check if this IP is registered
-    $ip_registered = ptp_is_ip_registered();
-    // Check if serial is valid
-    $serial_key_valid = ptp_is_valid_serial_key( get_option( '_ptp_sk' ) );
-
-    // If both are not true, deactivate
-    if ( !$ip_registered['success'] || !$serial_key_valid['success'] )
-        return true;
-
-    return true;
-}
+function ptp_verify() {}
 
 /**
  * Get the term parent
